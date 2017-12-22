9859d0ae5c5b8388ce9e3bd6d9b99797e7683aeb
Turkcod <34769265+Turkcod@users.noreply.github.com>
2017-12-22 14:05:34 +0300

    wpposter

---
+++ wpPoster/wpPoster/Main.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace wpPoster
+{
+    partial class Main
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendButton = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.keywords = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.category = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.body = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // sendButton
+            // 
+            this.sendButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.sendButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sendButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sendButton.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.sendButton.Location = new System.Drawing.Point(226, 311);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(116, 38);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "Send to Wordpress";
+            this.sendButton.UseVisualStyleBackColor = false;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // title
+            // 
+            this.title.Location = new System.Drawing.Point(91, 31);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(436, 22);
+            this.title.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Orange;
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Title";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Orange;
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(25, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Keywords";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // keywords
+            // 
+            this.keywords.Location = new System.Drawing.Point(91, 209);
+            this.keywords.Name = "keywords";
+            this.keywords.Size = new System.Drawing.Size(436, 22);
+            this.keywords.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Orange;
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(25, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Category";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // category
+            // 
+            this.category.Location = new System.Drawing.Point(91, 254);
+            this.category.Name = "category";
+            this.category.Size = new System.Drawing.Size(189, 22);
+            this.category.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Orange;
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(25, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Body";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // body
+            // 
+            this.body.Location = new System.Drawing.Point(91, 74);
+            this.body.Name = "body";
+            this.body.Size = new System.Drawing.Size(436, 110);
+            this.body.TabIndex = 9;
+            this.body.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 375);
+            this.Controls.Add(this.body);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.category);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.keywords);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.sendButton);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Main";
+            this.Text = "Wordpress Sender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox keywords;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox category;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox body;
+    }
+}
+
---
+++ wpPoster/wpPoster/Main.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JoeBlogs;
+
+namespace wpPoster
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        public int postWordpress(string title_, string body_, string category_, string keywords_)
+        {
+            try
+            {
+                // you should change constructor values to yours info
+                WordPressWrapper wp = new WordPressWrapper("http://mywordpress.com/xmlrpc.php", "USERNAME","PASSWORD",1);
+
+                // our keywords count can more than 1, so we can add whole keywords to string list
+                string[] keyList;
+                if (keywords_.Contains(' ')) keyList = keywords_.Split(',');
+                else
+                {
+                    keyList = new string[1];
+                    keyList[0] = keywords_;
+                }
+                // we created new object in Post Class and fill required post fields through this object
+                var post = new Post();
+                post.DateCreated = DateTime.Now;
+
+                post.Title = title_;
+                post.Tags = keyList;
+                post.Categories = new string[] { category_ };
+                post.Body = body_;
+                int index = wp.NewPost(post, true);
+                return index; // it will publish post and return post id
+            }
+            catch (Exception ex)
+            {
+                // if there is a mistake, the catch block will show message to us
+                MessageBox.Show(ex.Message);
+            }
+            return 0; // if there is a mistake it will return 0
+        }
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            // it will call our function and get post id
+            int postId = postWordpress(title.Text, body.Text, category.Text, keywords.Text);
+            MessageBox.Show(postId.ToString());
+        }
+    }
+}
